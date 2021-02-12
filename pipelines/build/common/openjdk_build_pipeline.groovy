e33c28bdb2ce5fe7aa43ab19a4ffc6de8f0c0c79
Morgan Davies <morgandavies2020@gmail.com>
2021-02-12 12:25:24 +0000

    Checkout with scm

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -952,7 +952,7 @@ class Build {
                     if (useAdoptShellScripts) {
                         repoHandler.checkoutAdopt()
                     } else {
-                        repoHandler.checkoutUser()
+                        context.checkout context.scm
                     }
                     // Perform a git clean outside of checkout to avoid the Jenkins enforced 10 minute timeout
                     // https://github.com/AdoptOpenJDK/openjdk-infrastructure/issues/1553
