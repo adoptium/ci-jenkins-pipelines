d47fe475a6c35349554cf0bd125c7c8fbea160c6
Sophia Guo <sophia.gwf@gmail.com>
2021-03-05 12:18:55 -0500

    Remove sanity.external group for now
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- pipelines/build/common/build_base_file.groovy
+++ pipelines/build/common/build_base_file.groovy
@@ -83,8 +83,7 @@ class Builder implements Serializable {
     final List<String> weekly = [
         'extended.openjdk',
         'extended.perf',
-        'special.functional',
-        'sanity.external'
+        'special.functional'
     ]
 
     // Declare timeouts for each critical stage (unit is HOURS)
@@ -134,7 +133,7 @@ class Builder implements Serializable {
             buildArgs += " " + additionalBuildArgs
         }
 
-        def testList = getTestList(platformConfig, variant)
+        def testList = getTestList(platformConfig)
 
         def platformCleanWorkspaceAfterBuild = getCleanWorkspaceAfterBuild(platformConfig)
 
@@ -215,7 +214,7 @@ class Builder implements Serializable {
     Get the list of tests to run from the build configurations.
     We run different test categories depending on if this build is a release or nightly. This function parses and applies this to the individual build config.
     */
-    List<String> getTestList(Map<String, ?> configuration, variant) {
+    List<String> getTestList(Map<String, ?> configuration) {
         List<String> testList = []
         /*
         * No test key or key value is test: false  --- test disabled
@@ -249,14 +248,7 @@ class Builder implements Serializable {
         }
         
         testList.unique()
-        
-        //Disable external tests on non-docker available platforms and limit external tests on hotspot and openj9 impl.
-        if (testList.contains('sanity.external')) {
-            if ((configuration.arch != 'ppc64le' && configuration.arch != 's390x' && !(configuration.os == 'linux' && configuration.arch == 'x64')) || (variant == 'corretto' || variant == 'dragonwell' || variant == 'bisheng')) {
-                testList.remove(testList.indexOf('sanity.external'))
-            }
-        }
-        
+
         return testList
     }
 
