7762fbef1dcc3c1a37d2c9494664e8b40660ea6d
Morgan Davies <morgandavies2020@gmail.com>
2021-03-16 22:32:36 +0000

    indentation

--- tools/code-tools/asmtools.sh
+++ tools/code-tools/asmtools.sh
@@ -9,60 +9,60 @@ function generateArtifact() {
     echo "tag=$tag"
     echo "version=$version"
 
-	artifact=asmtools-$version
+    artifact=asmtools-$version
 
-	tagName=$(git describe --tags `git rev-list --tags --max-count=1`)
-	echo "Tag: ${tagName}"
+    tagName=$(git describe --tags `git rev-list --tags --max-count=1`)
+    echo "Tag: ${tagName}"
 
-	git checkout ${tagName}
+    git checkout ${tagName}
 
-	# In WORKSPACE until here
-	echo "Moving into build..."
-	cd build
+    # In WORKSPACE until here
+    echo "Moving into build..."
+    cd build
 
     perl -p -i -e 's/"9"/"1.8"/g' build.xml
 
-	export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
+    export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 
-	echo "Building asmtools"
+    echo "Building asmtools"
 
     ant build
 
-	# WORKSPACE/asmtools/build/BUILD_DIR
-	echo "Moving down to ${BUILD_DIR}"
-	cd "${BUILD_DIR}"
+    # WORKSPACE/asmtools/build/BUILD_DIR
+    echo "Moving down to ${BUILD_DIR}"
+    cd "${BUILD_DIR}"
 
     echo "Asmtools artifact = $artifact"
 
     echo "Copying release/lib/asmtools*.jar file to ../"
     cp release/lib/asmtools*.jar ..
 
-	# WORKSPACE/asmtools/build/BUILD_DIR/dist
+    # WORKSPACE/asmtools/build/BUILD_DIR/dist
     echo "Moving into $artifact-build/dist"
     cd dist
 
-	echo "tar-ing ${artifact}.zip into ${artifact}.tar"
-	tar fcv "$artifact.tar" *.zip
+    echo "tar-ing ${artifact}.zip into ${artifact}.tar"
+    tar fcv "$artifact.tar" *.zip
 
-	echo "Moving ${artifact}.tar to ../.."
-	mv "$artifact.tar" ../..
-	# WORKSPACE/asmtools/build/
-	cd ../..
+    echo "Moving ${artifact}.tar to ../.."
+    mv "$artifact.tar" ../..
+    # WORKSPACE/asmtools/build/
+    cd ../..
 
-	echo "gzipping ${artifact}.tar"
-	gzip -9 -f "${artifact}.tar"
+    echo "gzipping ${artifact}.tar"
+    gzip -9 -f "${artifact}.tar"
 
-	if [ -d "$artifact-build" ];
-	then
-		mv "$artifact-build" asmtools/
-	fi
+    if [ -d "$artifact-build" ];
+    then
+        mv "$artifact-build" asmtools/
+    fi
 
     echo "Creating checksums for asmtools.jar and ${artifact}.tar.gz"
     sha256sum asmtools.jar > asmtools.jar.sha256sum.txt
     sha256sum "${artifact}.tar.gz" > "${artifact}.tar.gz.sha256sum.txt"
 
     echo "Moving into asmtools"
-	cd asmtools
+    cd asmtools
 }
 
 cd asmtools
