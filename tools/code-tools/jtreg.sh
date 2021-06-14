f27030bab9fa78e088449008a2736939a65cb294
Sophia Guo <sophia.gwf@gmail.com>
2021-06-14 11:03:12 -0400

    Fix jtreg build, clean redundant code
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- tools/code-tools/jtreg.sh
+++ tools/code-tools/jtreg.sh
@@ -3,16 +3,11 @@
 
 set -eu
 
-readonly TIP_VERSION='4.2.0'
-readonly TESTNG_VERSION='6.9.5'
-readonly JUNIT_VERSION='4.10'
-readonly JCOMMANDER_VERSION='1.48'
-export ASMTOOLS_HOME=""
+readonly JTREG_5='jtreg5.1-b01'
+readonly JTREG_6='jtreg-6+1'
 
 export WORKSPACE="$WORKSPACE/jtreg"
 cd "$WORKSPACE"
-export JCOV_HOME
-export JTHARNESS_HOME
 
 function checkWorkspaceVar()
 {
@@ -24,84 +19,6 @@ function checkWorkspaceVar()
     echo "$WORKSPACE"
   else
     echo "WORKSPACE variable is already set to '$WORKSPACE'"
-  fi
-}
-
-function downloadJavaHelp()
-{
-  echo -n 'Checking for downloaded javahelp...'
-  if [ -d 'jh2.0' ] ; then
-    echo ' found.'
-  else
-    echo ' not found, downloading:'
-    rm -f javahelp2_0_05.zip
-    # wget http://download.java.net/javadesktop/javahelp/javahelp2_0_05.zip
-    wget https://github.com/glub/secureftp/raw/master/contrib/javahelp2_0_05.zip
-    unzip -o javahelp2_0_05.zip
-  fi
-}
-
-function downloadJUnit()
-{
-  echo -n 'Checking for downloaded junit...'
-  if [ -s 'junit/junit.jar' ] && [ -s 'junit/'${JUNIT_VERSION} ]; then
-    echo ' found.'
-  else
-    echo ' not found, downloading:'
-    # shellcheck disable=SC2035
-    ( mkdir -p 'junit' && cd 'junit' && rm -rf * && wget http://repo1.maven.org/maven2/junit/junit/${JUNIT_VERSION}/junit-${JUNIT_VERSION}.jar && mv junit-${JUNIT_VERSION}.jar junit.jar && touch ${JUNIT_VERSION})
-  fi
-}
-
-function downloadTestNG()
-{
-  echo -n 'Checking for downloaded testng...'
-  if [ -s 'testng/testng.jar' ] && [ -s 'testng/'${TESTNG_VERSION} ]; then
-    echo ' found.'
-  else
-    echo ' not found, downloading:'
-    ( rm -rf testng* && wget http://jcenter.bintray.com/org/testng/testng/$TESTNG_VERSION/testng-$TESTNG_VERSION.jar && mkdir -p testng && mv testng-$TESTNG_VERSION.jar testng/testng.jar && touch ${TESTNG_VERSION})
-  fi
-}
-
-function downloadJCov()
-{
-  JCOV_SHORT_VERSION="3.0"
-  JCOV_FULL_VERSION="jcov-${JCOV_SHORT_VERSION}-beta-2"
-  tar -zxvf ${JCOV_FULL_VERSION}.tar.gz
-  mv JCOV_BUILD jcov
-  mv jcov/jcov_${JCOV_SHORT_VERSION} jcov/lib
-  JCOV_HOME="$( cd jcov && pwd )"
-}
-
-function downloadAsmTools()
-{
-  TGZ_EXTENSION="tar.gz"
-  ASMTOOLS_ARTIFACT=$(ls asmtools*.tar.gz)
-  ASMTOOLS_ARTIFACT=$(echo "${ASMTOOLS_ARTIFACT%.*}")
-  ASMTOOLS_ARTIFACT=$(echo "${ASMTOOLS_ARTIFACT%.*}")
-  tar -xzvf "${ASMTOOLS_ARTIFACT}.${TGZ_EXTENSION}"
-  ## tar contains zip for some reason:
-  unzip -o "${ASMTOOLS_ARTIFACT}.zip"
-  mv "${ASMTOOLS_ARTIFACT}" asmtools
-  ASMTOOLS_HOME="$( cd asmtools && pwd )"
-}
-
-function downloadJTHarness()
-{
-  export JTHARNESS=jtharness
-  tar -zxvf ${JTHARNESS}.tar.gz
-  JTHARNESS_HOME="$( cd jtharness && pwd )"
-}
-
-function downloadJCommander()
-{
-  echo -n 'Checking for downloaded jcommander...'
-  if [ -s 'jcommander/jcommander.jar' ] [ -s 'jcommander/'${JCOMMANDER_VERSION} ]; then
-    echo ' found.'
-  else
-    echo ' not found, downloading:'
-    ( rm -rf 'jcommander' && mkdir 'jcommander' && cd 'jcommander' && wget http://repo1.maven.org/maven2/com/beust/jcommander/${JCOMMANDER_VERSION}/jcommander-${JCOMMANDER_VERSION}.jar && mv jcommander-${JCOMMANDER_VERSION}.jar jcommander.jar && touch ${JCOMMANDER_VERSION})
   fi
 }
 
@@ -117,37 +34,47 @@ function clearWorkspace()
 }
 
 buildJTReg()
-{
+{ 
+  version="jtregtip"
+  if [ "$#" -eq 1 ]; then
+    version=$1
+    if [ "$1" == "jtreg5.1-b01" ]; then
+      export BUILD_NUMBER="b01"
+      export BUILD_VERSION="5.1"
+    fi
+    git checkout $version
+  else
+    git checkout master
+  fi
+
   echo ""
   echo "***********************************************"
-  echo "Building $1..."
+  echo "Building JTREG $version..."
   echo "***********************************************"
   (
-   versionNumber=$2
-   buildNumber=$3
-   echo "VersionNumber: ${versionNumber}"
-   echo "BuildNumber: ${buildNumber}"
-   echo "JAVA_HOME: $JAVA_HOME"
+    echo "JAVA_HOME: $JAVA_HOME"
+    echo "Changing into $WORKSPACE"
+    cd "$WORKSPACE"
+    echo "Currently in $PWD"
 
-   echo "Changing into $WORKSPACE"
-   cd "$WORKSPACE"
-   echo "Currently in $PWD"
+    echo "Removing contents of build folder"
+    rm -fr build || true
+    if [ "$version" == "jtreg5.1-b01" ]; then
+      chmod +x make/build-all.sh
+      make/build-all.sh "$JAVA_HOME"
+    else
+      chmod +x make/build.sh
+      make/build.sh --jdk "$JAVA_HOME"
+    fi
+   
+    cd build/images
 
-   echo "Removing contents of build folder"
-   rm -fr build || true
+    createWin32FolderWithJTRegBinaries
 
-#   cd make
-   chmod +x make/build-all.sh
-   make/build-all.sh "$JAVA_HOME"
-
-   cd build/images
-
-   createWin32FolderWithJTRegBinaries
-
-   tar -cvf jtreg.tar jtreg
-   gzip -9 jtreg.tar
-   mv jtreg.tar.gz "$WORKSPACE/jtreg-$versionNumber-$buildNumber.tar.gz"
-   createChecksum "$WORKSPACE/jtreg-$versionNumber-$buildNumber.tar.gz" "$WORKSPACE"
+    tar -cvf jtreg.tar jtreg
+    gzip -9 jtreg.tar
+    mv jtreg.tar.gz "$WORKSPACE/$version.tar.gz"
+    createChecksum "$WORKSPACE/$version.tar.gz" "$WORKSPACE"
   )
 }
 
@@ -155,23 +82,6 @@ createWin32FolderWithJTRegBinaries()
 {
    mkdir -p jtreg/win32
    cp -fr jtreg/bin jtreg/win32/
-}
-
-buildJTRegTip()
-{
-  buildJTReg "the tip" "$TIP_VERSION" "tip"
-}
-
-buildJTRegLastTag()
-{
-  tagName=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-    echo "Tag: ${tagName}"
-  versionAndBuildNumber=$(echo "${tagName}"| awk '{split($0,a,"jtreg"); print a[2]}')
-  versionNumber=$(echo "${versionAndBuildNumber}" | awk '{split($0,a,"-"); print a[1]}')
-  buildNumber=$(echo "${versionAndBuildNumber}" | awk '{split($0,a,"-"); print a[2]}')
-  git checkout "${tagName}"
-
-  buildJTReg "last tag" "$versionNumber" "$buildNumber"
 }
 
 createChecksum() {
@@ -184,18 +94,10 @@ createChecksum() {
     sha256sum "${ARCHIVE_FULL_PATH}" > "${DESTINATION}/${ARCHIVE_NAME}.sha256sum.txt"
 }
 
-runBasicJtregSanityTest() {
-    make "$(pwd)"/Basic.othervm.ok "$(pwd)"/Basic.agentvm.ok
-}
-
-runJtregSanitySelfTests() {
-    make -j 4 test
-}
-
 checkWorkspaceVar
 clearWorkspace
-
 echo 'Starting build process...'
-buildJTRegTip
-buildJTRegLastTag
+buildJTReg "$JTREG_5"
+buildJTReg "$JTREG_6"
+buildJTReg
 echo '...finished with build process.'
