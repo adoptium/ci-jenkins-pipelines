a91ad4987050459098c9dad1673852587514b807
Martijn Verburg <martijnverburg@gmail.com>
2021-09-15 15:49:51 +0100

    Ensure that Windows builds get built and tested on a ZGC supported version of the O/S

--- pipelines/jobs/configurations/jdk18_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk18_pipeline_config.groovy
@@ -39,7 +39,7 @@ class Config18 {
         x64Windows: [
                 os                  : 'windows',
                 arch                : 'x64',
-                additionalNodeLabels: 'win2016&&vs2019',
+                additionalNodeLabels: 'win2019&&vs2019',
                 test                : 'default'
         ],
 
