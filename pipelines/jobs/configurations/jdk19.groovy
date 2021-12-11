926fbff4e7aeb35a77c03f07d52eb4104342e7b1
George Adams <georgeadams1995@gmail.com>
2021-12-11 16:20:28 +0000

    bump JDK tip version to JDK19 (#220)

---
+++ pipelines/jobs/configurations/jdk19.groovy
@@ -0,0 +1,50 @@
+targetConfigurations = [
+        "x64Mac"      : [
+                "hotspot"
+        ],
+        "x64Linux"    : [
+                "hotspot"
+        ],
+        "x64AlpineLinux" : [
+                "hotspot"
+        ],
+        "x64Windows"  : [
+                "hotspot"
+        ],
+        "x32Windows"  : [
+                "hotspot"
+        ],
+        "ppc64Aix"    : [
+                "hotspot"
+        ],
+        "ppc64leLinux": [
+                "hotspot"
+        ],
+        "s390xLinux"  : [
+                "hotspot"
+        ],
+        "aarch64Linux": [
+                "hotspot"
+        ],
+        "aarch64Mac": [
+                "hotspot"
+        ],
+        "arm32Linux"  : [
+                "hotspot"
+        ]
+]
+
+// 23:30 Tue, Thur
+triggerSchedule_nightly="TZ=UTC\n30 23 * * 2,4"
+// 23:30 Sat
+triggerSchedule_weekly="TZ=UTC\n30 23 * * 6"
+
+// scmReferences to use for weekly release build
+weekly_release_scmReferences=[
+        "hotspot"        : "",
+        "openj9"         : "",
+        "corretto"       : "",
+        "dragonwell"     : ""
+]
+
+return this
---
+++ pipelines/jobs/configurations/jdk19_pipeline_config.groovy
@@ -0,0 +1,128 @@
+class Config19 {
+  final Map<String, Map<String, ?>> buildConfigurations = [
+        x64Mac    : [
+                os                  : 'mac',
+                arch                : 'x64',
+                additionalNodeLabels: 'macos10.14',
+                test                : 'default',
+                configureArgs       : '--enable-dtrace'
+        ],
+
+        x64Linux  : [
+                os                  : 'linux',
+                arch                : 'x64',
+                dockerImage: [
+                        hotspot     : 'adoptopenjdk/centos6_build_image',
+                        openj9      : 'adoptopenjdk/centos7_build_image'
+                ],
+                dockerFile: [
+                        openj9      : 'pipelines/build/dockerFiles/cuda.dockerfile'
+                ],
+                test                : 'default',
+                additionalTestLabels: [
+                        openj9      : '!(centos6||rhel6)'
+                ],
+                configureArgs       : [
+                        "openj9"    : '--enable-dtrace --enable-jitserver',
+                        "hotspot"   : '--enable-dtrace'
+                ]
+        ],
+
+        x64AlpineLinux  : [
+                os                  : 'alpine-linux',
+                arch                : 'x64',
+                dockerImage         : 'adoptopenjdk/alpine3_build_image',
+                test                : 'default',
+                configureArgs       : '--enable-headless-only=yes'
+        ],
+
+        x64Windows: [
+                os                  : 'windows',
+                arch                : 'x64',
+                additionalNodeLabels: 'win2012&&vs2017',
+                test                : 'default'
+        ],
+
+        // TODO: Enable testing (https://github.com/adoptium/ci-jenkins-pipelines/issues/77)
+        aarch64Windows: [
+                os                  : 'windows',
+                arch                : 'aarch64',
+                crossCompile        : 'x64',
+                buildArgs           : '--cross-compile',
+                additionalNodeLabels: 'win2016&&vs2019',
+                test                : [
+                        nightly: [],
+                        weekly : []
+                ]
+        ],
+
+
+        x32Windows: [
+                os                  : 'windows',
+                arch                : 'x86-32',
+                additionalNodeLabels: 'win2012&&vs2017',
+                buildArgs           : [
+                        hotspot : '--jvm-variant client,server'
+                ],
+                test                : 'default'
+        ],
+
+        ppc64Aix    : [
+                os                  : 'aix',
+                arch                : 'ppc64',
+                additionalNodeLabels: [
+                        hotspot: 'xlc16&&aix710',
+                        openj9:  'xlc16&&aix715'
+                ],
+                test                : 'default',
+                cleanWorkspaceAfterBuild: true
+        ],
+
+
+        s390xLinux    : [
+                os                  : 'linux',
+                arch                : 's390x',
+                test                : 'default',
+                configureArgs       : '--enable-dtrace'
+        ],
+
+        ppc64leLinux    : [
+                os                  : 'linux',
+                arch                : 'ppc64le',
+                additionalNodeLabels: 'centos7',
+                test                : 'default',
+                configureArgs       : [
+                        "hotspot"     : '--enable-dtrace',
+                        "openj9"      : '--enable-dtrace --enable-jitserver'
+                ]
+
+        ],
+
+        aarch64Linux    : [
+                os                  : 'linux',
+                arch                : 'aarch64',
+                dockerImage         : 'adoptopenjdk/centos7_build_image',
+                test                : 'default',
+                configureArgs : '--enable-dtrace',
+                testDynamic          : false
+        ],
+        
+        aarch64Mac: [
+                os                  : 'mac',
+                arch                : 'aarch64',
+                additionalNodeLabels: 'macos11',
+                test                : 'default'
+        ],
+
+        arm32Linux    : [
+                os                  : 'linux',
+                arch                : 'arm',
+                test                : 'default',
+                configureArgs       : '--enable-dtrace'
+        ]
+  ]
+
+}
+
+Config19 config = new Config19()
+return config.buildConfigurations
