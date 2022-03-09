df8108b89af600f8cbb75abff5b7a6a8559cd502
¥eeSkywalker <56951646+YeeSkywalker@users.noreply.github.com>
2022-03-09 13:26:25 -0700

    Add SDK_RESOURCE=upstream (#265)

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -296,6 +296,7 @@ class Build {
                     context.build job: jobName,
                             propagate: false,
                             parameters: [
+                                    context.string(name: 'SDK_RESOURCE', value: "upstream"),
                                     context.string(name: 'UPSTREAM_JOB_NUMBER', value: "${env.BUILD_NUMBER}"),
                                     context.string(name: 'UPSTREAM_JOB_NAME', value: "${env.JOB_NAME}"),
                                     context.string(name: 'JDK_VERSION', value: "${jobParams.JDK_VERSIONS}"),
