5163d29bef5ffef9bd24282a233a293b46d97858
Wen Zhou <wenzhou@redhat.com>
2022-04-13 12:25:20 +0200

    fix: typo

--- docs/generateBuildMatrix.sh
+++ docs/generateBuildMatrix.sh
@@ -3,12 +3,12 @@
 # Generates markdown table of build job status
 
 allPlatforms=("jdk8u" "jdk11u" "jdk17u" "jdk18u" "jdk")
-buidFile="/tmp/build.txt"
+buildFile="/tmp/build.txt"
 buildJobFile="/tmp/build_jobs.txt"
 
-if [[ -f ${buidFile} ]]; then
-  echo "Removing previous ${buidFile} ${buildJobFile} files"
-  rm ${buidFile}
+if [[ -f ${buildFile} ]]; then
+  echo "Removing previous ${buildFile} ${buildJobFile} files"
+  rm ${buildFile}
   rm ${buildJobFile}
 fi
 
@@ -20,33 +20,33 @@ done
 # The sed command fails on Mac OS X, but those users can install gnu-sed
 echo "This will take a few minutes to complete."
 # Header row
-echo -n "| Platform |" > ${buidFile}
+echo -n "| Platform |" > ${buildFile}
 for p in ${allPlatforms[@]};do
-  echo -n " ${p} |" | sed -e 's/jdk/Java /;s/u//;s/  / HEAD/' >> ${buidFile}
+  echo -n " ${p} |" | sed -e 's/jdk/Java /;s/u//;s/  / HEAD/' >> ${buildFile}
 done
-# Delimer row
-echo -n $'\n|------' >> ${buidFile} # to match Platform column
+# Delimiter row
+echo -n $'\n|------' >> ${buildFile} # to match Platform column
 for i in ${allPlatforms[@]} ;do
-  echo -n "|----" >> ${buidFile}
+  echo -n "|----" >> ${buildFile}
 done
-echo "|" >> ${buidFile}
+echo "|" >> ${buildFile}
 
 cat ${buildJobFile}| cut -d'/' -f2 | sed -r 's/jdk[0-9]*u?\-//g' | sort | uniq | while read buildName;
 do
     # buildName should be of the form: aix-ppc64-hotspot
-    echo -n "| ${buildName} | " >> ${buidFile}
+    echo -n "| ${buildName} | " >> ${buildFile}
     for i in ${allPlatforms[@]};
     do
         code=$(curl -L -s -o /dev/null -w "%{http_code}" "https://ci.adoptopenjdk.net/job/build-scripts/job/jobs/job/${i}/job/${i}-${buildName}")
         if [[ ${code} = 200 ]]; then
             echo -n "[![Build Status](https://ci.adoptopenjdk.net/buildStatus/icon?job=build-scripts/jobs/${i}/${i}-${buildName})](https://ci.adoptopenjdk.net/job/build-scripts/job/jobs/job/${i}/job/${i}-${buildName})" >> "/tmp/build.txt"
         else
-            echo -n "N/A" >> ${buidFile}
+            echo -n "N/A" >> ${buildFile}
         fi
 
-        echo -n " | " >> ${buidFile}
+        echo -n " | " >> ${buildFile}
     done
-    echo "" >> ${buidFile}
+    echo "" >> ${buildFile}
 done
 
-echo "Complete - markdown out has been generated in ${buidFile}"
+echo "Complete - markdown out has been generated in ${buildFile}"
