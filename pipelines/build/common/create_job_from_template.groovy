10eecb6596894b34b3ee0899b4370a0453fe96a1
Wen Zhou <wenzhou@redhat.com>
2022-05-12 16:35:33 +0200

    Revert "feat: enable --create-sbom (in temurin-build) by passing new BUILD_ARGS"
    This reverts commit 5b95f3d6751a82c939c06a8e6dfe94af9bf35d45.

--- pipelines/build/common/create_job_from_template.groovy
+++ pipelines/build/common/create_job_from_template.groovy
@@ -116,7 +116,6 @@ pipelineJob("$buildFolder/$JOB_NAME") {
                 <dt><strong>ENABLE_TESTS</strong></dt><dd>Run tests</dd>
                 <dt><strong>ENABLE_TESTDYNAMICPARALLEL</strong></dt><dd>Run parallel</dd>
                 <dt><strong>ENABLE_INSTALLERS</strong></dt><dd>Run installers</dd>
-                <dt><strong>ENABLE_SBOM</strong></dt><dd>Run create cyclonedxlib and generate SBOM</dd>
                 <dt><strong>ENABLE_SIGNER</strong></dt><dd>Run signer</dd>
                 <dt><strong>CLEAN_WORKSPACE</strong></dt><dd>Wipe out workspace before build</dd>
                 <dt><strong>CLEAN_WORKSPACE_AFTER</strong></dt><dd>Wipe out workspace after build</dd>
