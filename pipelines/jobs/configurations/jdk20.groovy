82a4193d2f62ce5d6f9be9525dd37a242695eada
Andrew Leonard <anleonar@redhat.com>
2022-06-23 17:12:01 +0100

    Add jdk20 pipeline config
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

---
+++ pipelines/jobs/configurations/jdk20.groovy
@@ -0,0 +1,59 @@
+targetConfigurations = [
+        "x64Mac"      : [
+                "temurin"
+        ],
+        "x64Linux"    : [
+                "temurin"
+        ],
+        "x64AlpineLinux" : [
+                "temurin"
+        ],
+        "x64Windows"  : [
+                "temurin"
+        ],
+        "x32Windows"  : [
+                "temurin"
+        ],
+        "ppc64Aix"    : [
+                "temurin"
+        ],
+        "ppc64leLinux": [
+                "temurin"
+        ],
+        "s390xLinux"  : [
+                "temurin"
+        ],
+        "aarch64Linux": [
+                "hotspot",
+                "temurin"
+        ],
+        "aarch64AlpineLinux" : [
+                "temurin"
+        ],
+        "aarch64Mac": [
+                "temurin"
+        ],
+        "arm32Linux"  : [
+                "temurin"
+        ],
+        "riscv64Linux"  : [
+                "temurin"
+        ]
+
+]
+
+// 03:30 Wed, Fri
+triggerSchedule_nightly="TZ=UTC\n30 03 * * 3,5"
+// 23:30 Sat
+triggerSchedule_weekly="TZ=UTC\n30 23 * * 6"
+
+// scmReferences to use for weekly release build
+weekly_release_scmReferences=[
+        "hotspot"        : "",
+        "temurin"        : "",
+        "openj9"         : "",
+        "corretto"       : "",
+        "dragonwell"     : ""
+]
+
+return this
---
+++ pipelines/jobs/configurations/jdk20_pipeline_config.groovy
@@ -0,0 +1,185 @@
+class Config20 {
+  final Map<String, Map<String, ?>> buildConfigurations = [
+        x64Mac    : [
+                os                  : 'mac',
+                arch                : 'x64',
+                additionalNodeLabels: 'macos10.14',
+                additionalTestLabels: [
+                        openj9      : '!sw.os.osx.10_11'
+                ],
+                test                : 'default',
+                configureArgs       : '--enable-dtrace',
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+        x64Linux  : [
+                os                  : 'linux',
+                arch                : 'x64',
+                dockerImage: [
+                        temurin     : 'adoptopenjdk/centos6_build_image',
+                        openj9      : 'adoptopenjdk/centos7_build_image'
+                ],
+                dockerFile: [
+                        openj9      : 'pipelines/build/dockerFiles/cuda.dockerfile'
+                ],
+                test                : 'default',
+                additionalTestLabels: [
+                        openj9      : '!(centos6||rhel6)'
+                ],
+                configureArgs       : [
+                        "openj9"    : '--enable-dtrace --enable-jitserver',
+                        "temurin"   : '--enable-dtrace'
+                ],
+                buildArgs           : [
+                        "temurin"   : '--create-source-archive --create-jre-image'
+                ]
+        ],
+
+        x64AlpineLinux  : [
+                os                  : 'alpine-linux',
+                arch                : 'x64',
+                dockerImage         : 'adoptopenjdk/alpine3_build_image',
+                test                : 'default',
+                configureArgs       : '--enable-headless-only=yes',
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+        aarch64AlpineLinux  : [
+                os                  : 'alpine-linux',
+                arch                : 'aarch64',
+                dockerImage         : 'adoptopenjdk/alpine3_build_image',
+                test                : 'default',
+                configureArgs       : '--enable-headless-only=yes',
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+        
+        x64Windows: [
+                os                  : 'windows',
+                arch                : 'x64',
+                additionalNodeLabels: 'win2012&&vs2019',
+                test                : 'default',
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+        // TODO: Enable testing (https://github.com/adoptium/ci-jenkins-pipelines/issues/77)
+        aarch64Windows: [
+                os                  : 'windows',
+                arch                : 'aarch64',
+                crossCompile        : 'x64',
+                buildArgs           : '--cross-compile',
+                additionalNodeLabels: 'win2016&&vs2019',
+                test                : [
+                        nightly: [],
+                        weekly : []
+                ],
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+
+        x32Windows: [
+                os                  : 'windows',
+                arch                : 'x86-32',
+                additionalNodeLabels: 'win2012&&vs2019',
+                test                : 'default',
+                buildArgs           : [
+                        "temurin"   : '--jvm-variant client,server --create-jre-image'
+                ]
+        ],
+
+        ppc64Aix    : [
+                os                  : 'aix',
+                arch                : 'ppc64',
+                additionalNodeLabels: [
+                        temurin: 'xlc16&&aix720',
+                        openj9:  'xlc16&&aix715'
+                ],
+                test                : 'default',
+                cleanWorkspaceAfterBuild: true,
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+
+        s390xLinux    : [
+                os                  : 'linux',
+                arch                : 's390x',
+                test                : 'default',
+                configureArgs       : '--enable-dtrace',
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+        ppc64leLinux    : [
+                os                  : 'linux',
+                arch                : 'ppc64le',
+                additionalNodeLabels: 'centos7',
+                test                : 'default',
+                configureArgs       : [
+                        "temurin"     : '--enable-dtrace',
+                        "openj9"      : '--enable-dtrace --enable-jitserver'
+                ],
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+        aarch64Linux    : [
+                os                  : 'linux',
+                arch                : 'aarch64',
+                dockerImage         : 'adoptopenjdk/centos7_build_image',
+                test                : 'default',
+                configureArgs : '--enable-dtrace',
+                testDynamic          : false,
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+        
+        aarch64Mac: [
+                os                  : 'mac',
+                arch                : 'aarch64',
+                additionalNodeLabels: 'macos11',
+                test                : 'default',
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+
+        arm32Linux    : [
+                os                  : 'linux',
+                arch                : 'arm',
+                test                : 'default',
+                configureArgs       : '--enable-dtrace',
+                buildArgs           : [
+                        "temurin"   : '--create-jre-image'
+                ]
+        ],
+        riscv64Linux      :  [
+                os                   : 'linux',
+                arch                 : 'riscv64',
+                configureArgs        : '--enable-dtrace --with-native-debug-symbols=none',
+                buildArgs            : '-r https://github.com/openjdk/riscv-port -b riscv-port --custom-cacerts false --disable-adopt-branch-safety',
+                test                : [
+                        nightly: ['sanity.openjdk'],
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf']
+                ]
+        ]
+
+  ]
+
+}
+
+Config20 config = new Config20()
+return config.buildConfigurations
