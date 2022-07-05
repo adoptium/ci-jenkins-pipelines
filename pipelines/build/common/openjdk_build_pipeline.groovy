019fbf45a1e8f965f3ac5e0b400e64c8d897e1ff
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2022-07-05 17:45:16 +0100

    Remove extra GPG sign job

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -534,34 +534,6 @@ class Build {
                 }
             }
         }
-        context.stage("GPG sign") {
-
-            context.println "RUNNING sign_temurin_gpg for ${buildConfig.TARGET_OS}/${buildConfig.ARCHITECTURE} ..."
-           
-            def params = [
-                context.string(name: 'UPSTREAM_JOB_NUMBER', value: "${env.BUILD_NUMBER}"),
-                context.string(name: 'UPSTREAM_JOB_NAME', value: "${env.JOB_NAME}"),
-                context.string(name: 'UPSTREAM_DIR', value: "workspace/target"),
-                ['$class': 'LabelParameterValue', name: 'NODE_LABEL', label: "built-in"]
-            ]
-
-            def signSHAsJob = context.build job: "build-scripts/release/sign_temurin_gpg",
-                propagate: true,
-                parameters: params
-
-            context.node('worker') {
-                context.sh "rm -f workspace/target/*.sig"
-                context.copyArtifacts(
-                    projectName: "build-scripts/release/sign_temurin_gpg",
-                    selector: context.specific("${signSHAsJob.getNumber()}"),
-                    filter: '**/*.sig',
-                    fingerprintArtifacts: true, 
-                    target: 'workspace/target/',
-                    flatten: true)
-                // Archive GPG signatures in Jenkins
-                context.archiveArtifacts artifacts: "workspace/target/*.sig"
-            }
-        }
     }
 
     /*
