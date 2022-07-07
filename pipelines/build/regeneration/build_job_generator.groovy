7dd226b143ff7a3e75dfe6ae264844fe708cad30
Wen Zhou <wenzhou@redhat.com>
2022-07-07 16:51:38 +0200

    fix: remove reference of import_lib.groovy from diagram

--- pipelines/build/regeneration/build_job_generator.groovy
+++ pipelines/build/regeneration/build_job_generator.groovy
@@ -17,6 +17,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/* pipeline script for job pipeline_jobs_generator_jdkX */
+
 String javaVersion = params.JAVA_VERSION
 String ADOPT_DEFAULTS_FILE_URL = "https://raw.githubusercontent.com/adoptium/ci-jenkins-pipelines/master/pipelines/defaults.json"
 String DEFAULTS_FILE_URL = (params.DEFAULTS_URL) ?: ADOPT_DEFAULTS_FILE_URL
