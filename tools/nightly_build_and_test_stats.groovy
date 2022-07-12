228b1bb0562e72d8d4cdb234464618f4eee86915
Andrew Leonard <31470007+andrew-m-leonard@users.noreply.github.com>
2022-07-12 11:52:27 +0100

    Prevent rating division by zero error if test job run but no testcases run (#366)
    * Prevent rating division by zero error if test job run but no testcases run
    
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>
    
    * Update tools/nightly_build_and_test_stats.groovy
    
    Co-authored-by: Martijn Verburg <martijnverburg@gmail.com>

--- tools/nightly_build_and_test_stats.groovy
+++ tools/nightly_build_and_test_stats.groovy
@@ -203,11 +203,15 @@ node ("worker") {
       totalBuildJobs += pipeline.buildJobNumber
       buildFailures += pipeline.buildJobFailure
       totalTestJobs += pipeline.testJobNumber
-      // Did tests run? (build may have failed)
+      // Did test jobs run? (build may have failed)
       if (pipeline.testJobNumber > 0) {
         numTestPipelines += 1
         // Pipeline Test % success rating: %(SucceededOrUnstable) - %(FailedTestCases)
-        nightlyTestSuccessRating += ( ((pipeline.testJobNumber-pipeline.testJobFailure)*100/pipeline.testJobNumber) - (pipeline.testCaseFailed*100/(pipeline.testCasePassed+pipeline.testCaseFailed)) )
+        nightlyTestSuccessRating += ( ((pipeline.testJobNumber-pipeline.testJobFailure)*100/pipeline.testJobNumber) )
+        // Did test cases run?
+        if ((pipeline.testCasePassed + pipeline.testCaseFailed) > 0) {
+            nightlyTestSuccessRating -= (pipeline.testCaseFailed*100/(pipeline.testCasePassed+pipeline.testCaseFailed))
+        }
       }
     }
     // Average test success rating across all pipelines
