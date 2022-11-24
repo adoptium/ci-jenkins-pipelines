5fc7f365bc4b0f69f38da3d779e7bac232ba7939
Wen Zhou <wenzhou@redhat.com>
2022-11-24 17:44:51 +0100

    update: add release build config map

---
+++ pipelines/jobs/configurations/jdk8u_release.groovy
@@ -0,0 +1,40 @@
+targetConfigurations = [
+        'x64Mac'        : [
+                'temurin'
+        ],
+        'x64Linux'      : [
+                'temurin'
+        ],
+        'x64AlpineLinux' : [
+                'temurin'
+        ],
+        'x32Windows'    : [
+                'temurin'
+        ],
+        'x64Windows'    : [
+                'temurin'
+        ],
+        'ppc64Aix'      : [
+                'temurin'
+        ],
+        'ppc64leLinux'  : [
+                'temurin'
+        ],
+        's390xLinux'    : [
+                'temurin'
+        ],
+        'aarch64Linux'  : [
+                'temurin'
+        ],
+        'arm32Linux'  : [
+                'temurin'
+        ],
+        'x64Solaris': [
+                'temurin'
+        ],
+        'sparcv9Solaris': [
+                'temurin'
+        ]
+]
+
+return this
