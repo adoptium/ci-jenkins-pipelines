84b92052826fa3551485563e00a7d815d0c7c1c9
Wen Zhou <wenzhou@redhat.com>
2022-12-13 11:47:31 +0100

    update: remove aarch64Windows from prototype first

--- pipelines/jobs/configurations/jdk20_prototype.groovy
+++ pipelines/jobs/configurations/jdk20_prototype.groovy
@@ -2,9 +2,11 @@ targetConfigurations = [
         'aarch64Linux': [
                 'hotspot'
         ],
+/*
         'aarch64Windows' : [
                 'temurin'
         ]
+*/
 ]
 
 return this
