aa7e476e88a3a542c0c710153f6ba3a3e9ad41d1
Wen Zhou <wenzhou@redhat.com>
2023-01-17 16:14:06 +0100

    fix: typo

--- pipelines/build/prTester/pr_test_pipeline.groovy
+++ pipelines/build/prTester/pr_test_pipeline.groovy
@@ -109,7 +109,7 @@ class PullRequestTestPipeline implements Serializable {
                     buildConfigurations,
                     testConfigurations,
                     DEFAULTS_JSON,
-                    ADOPT_DEFAULTS_JSON，
+                    ADOPT_DEFAULTS_JSON,
                     excludedBuilds,
                     900,
                     currentBuild,
