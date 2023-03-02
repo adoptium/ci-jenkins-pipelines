0c6e5d10e5c55b53df9ef9e6789f38f4d5b79ac9
Sophia Guo <sophia.gwf@gmail.com>
2023-03-02 15:11:03 -0500

    Add upstream job link for remote triggered job
    Set up direct information about the upstream job from remote triggered
    job side
    
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -526,7 +526,6 @@ class Build {
                          parallel = 'Dynamic'
                          num_machines = '2'
                     }
-
                     context.catchError {
                         context.triggerRemoteJob abortTriggeredJob: true,
                             blockBuildUntilComplete: false,
@@ -541,6 +540,7 @@ class Build {
                                                                     context.MapParameter(name: 'PIPELINE_DISPLAY_NAME', value: "${displayName}"),
                                                                     context.MapParameter(name: 'APPLICATION_OPTIONS', value: "${appOptions}"),
                                                                     context.MapParameter(name: 'LABEL_ADDITION', value: additionalTestLabel),
+                                                                    context.MapParameter(name: 'cause', value: "Remote triggered by job ${env.BUILD_URL}"), // Label is lowercase on purpose to map to the Jenkins target reporting system
                                                                     context.MapParameter(name: 'SETUP_JCK_RUN', value: "${setupJCKRun}")]),
                             remoteJenkinsName: 'temurin-compliance',
                             shouldNotFailBuild: true,
