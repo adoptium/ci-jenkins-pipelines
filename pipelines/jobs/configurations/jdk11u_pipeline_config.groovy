0cbcee9f8bc2266715caf389e1b3cb2edfbe13ee
Stewart X Addison <sxa@redhat.com>
2023-03-07 11:27:38 +0000

    Use rhel7_build_image for Linux/s390x builds
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

--- pipelines/jobs/configurations/jdk11u_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk11u_pipeline_config.groovy
@@ -103,6 +103,7 @@ class Config11 {
         s390xLinux    : [
                 os                  : 'linux',
                 arch                : 's390x',
+                dockerImage         : 'rhel7_build_image',
                 test                : 'default',
                 configureArgs       : '--enable-dtrace=auto',
                 buildArgs           : [
--- pipelines/jobs/configurations/jdk20_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk20_pipeline_config.groovy
@@ -87,6 +87,7 @@ class Config20 {
         s390xLinux    : [
                 os                  : 'linux',
                 arch                : 's390x',
+                dockerImage         : 'rhel7_build_image',
                 test                : 'default',
                 configureArgs       : '--enable-dtrace',
                 buildArgs           : [
