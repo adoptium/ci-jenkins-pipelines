5acbf4db49948df200da7f73ca82628936348c76
Scott Fryer <scfryer@redhat.com>
2023-03-08 08:58:07 +0000

    Update jdk20_release.groovy

--- pipelines/jobs/configurations/jdk20_release.groovy
+++ pipelines/jobs/configurations/jdk20_release.groovy
@@ -11,17 +11,19 @@ targetConfigurations = [
         'x64Windows'  : [
                 'temurin'
         ],
-        'ppc64Aix'    : [
+        'x32Windows'  : [
                 'temurin'
         ],
         'ppc64leLinux': [
+                'temurin'
+        ],
+        'ppc64Aix'    : [
                 'temurin'
         ],
         's390xLinux'  : [
                 'temurin'
         ],
         'aarch64Linux': [
-                'hotspot',
                 'temurin'
         ],
         'aarch64Mac': [
@@ -29,25 +31,7 @@ targetConfigurations = [
         ],
         'arm32Linux'  : [
                 'temurin'
-        ],
-        'riscv64Linux': [
-                'temurin'
         ]
-
-]
-
-// 03:30 Wed, Fri
-triggerSchedule_nightly = 'TZ=UTC\n30 03 * * 3,5'
-// 23:30 Sat
-triggerSchedule_weekly = 'TZ=UTC\n30 23 * * 6'
-
-// scmReferences to use for weekly release build
-weekly_release_scmReferences = [
-        'hotspot'        : '',
-        'temurin'        : '',
-        'openj9'         : '',
-        'corretto'       : '',
-        'dragonwell'     : ''
 ]
 
 return this
