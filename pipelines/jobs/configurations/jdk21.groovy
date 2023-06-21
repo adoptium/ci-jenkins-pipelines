cbe63a572843b50190b000dc1c5a06a9a9f06333
Stewart X Addison <sxa@redhat.com>
2023-06-21 16:22:26 +0100

    Disable nightly builds as they are superceded by ea
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

--- pipelines/jobs/configurations/jdk21.groovy
+++ pipelines/jobs/configurations/jdk21.groovy
@@ -33,7 +33,7 @@ targetConfigurations = [
 ]
 
 // 23:30 Mon, Wed, Fri
-triggerSchedule_nightly = 'TZ=UTC\n30 23 * * 1,3,5'
+// triggerSchedule_nightly = 'TZ=UTC\n30 23 * * 1,3,5'
 // 23:30 Sat
 triggerSchedule_weekly = 'TZ=UTC\n30 23 * * 6'
 
