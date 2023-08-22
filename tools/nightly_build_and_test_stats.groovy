ec5cc9dd3b845ff3e359dfcd8946772540fa0546
Andrew Leonard <anleonar@redhat.com>
2023-08-22 16:27:33 +0100

    Update daily status to support tag triggered builds
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- tools/nightly_build_and_test_stats.groovy
+++ tools/nightly_build_and_test_stats.groovy
@@ -112,7 +112,7 @@ echo "featureReleases = ${featureReleases}"
               // Verify the given release contains all the expected assets
               verifyReleaseContent(featureRelease, releaseName, status)
 echo "1"
-              def key = featureRelease
+              def key = "${featureRelease}"
               healthStatus[key] = status
 echo "2"
             }
