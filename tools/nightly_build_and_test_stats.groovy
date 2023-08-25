79abd13f8dfe1af2e7691340bcf9ceabe18d6e33
Andrew Leonard <anleonar@redhat.com>
2023-08-25 14:28:36 +0100

    Update daily status to support tag triggered builds
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- tools/nightly_build_and_test_stats.groovy
+++ tools/nightly_build_and_test_stats.groovy
@@ -81,12 +81,13 @@ def verifyReleaseContent(String version, String release, Map status) {
                 echo "    Verifying : $osarch"
 
                 def imagetypes = ["debugimage", "jdk", "jre", "sbom", "static-libs", "testimage"]
+                def filetypes
                 if ( "$osarch".contains("Windows")) {
-                    def filetypes = ["\\.msi", "\\.msi\\.json", "\\.msi\\.sha256\\.txt", "\\.msi\\.sig", "\\.zip", "\\.zip\\.json", "\\.zip\\.sha256\\.txt", "\\.zip\\.sig"]
+                    filetypes = ["\\.msi", "\\.msi\\.json", "\\.msi\\.sha256\\.txt", "\\.msi\\.sig", "\\.zip", "\\.zip\\.json", "\\.zip\\.sha256\\.txt", "\\.zip\\.sig"]
                 } else if ( "$osarch".contains("Mac")) {
-                    def filetypes = ["\\.pkg", "\\.pkg\\.json", "\\.pkg\\.sha256\\.txt", "\\.pkg\\.sig", "\\.tar\\.gz", "\\.tar\\.gz\\.json", "\\.tar\\.gz\\.sha256\\.txt", "\\.tar\\.gz\\.sig"]
+                    filetypes = ["\\.pkg", "\\.pkg\\.json", "\\.pkg\\.sha256\\.txt", "\\.pkg\\.sig", "\\.tar\\.gz", "\\.tar\\.gz\\.json", "\\.tar\\.gz\\.sha256\\.txt", "\\.tar\\.gz\\.sig"]
                 } else {
-                    def filetypes = ["\\.tar\\.gz", "\\.tar\\.gz\\.json", "\\.tar\\.gz\\.sha256\\.txt", "\\.tar\\.gz\\.sig"]
+                    filetypes = ["\\.tar\\.gz", "\\.tar\\.gz\\.json", "\\.tar\\.gz\\.sha256\\.txt", "\\.tar\\.gz\\.sig"]
                 }
 
                 imagetypes.each { image ->
