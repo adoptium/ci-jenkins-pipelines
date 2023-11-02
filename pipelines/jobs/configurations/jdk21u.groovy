f4e4676206d93508b40dab0699c089c8e32cf6b4
Andrew Leonard <31470007+andrew-m-leonard@users.noreply.github.com>
2023-11-02 22:32:30 +0000

    Build jdk21u HEAD weekly (#834)
    * Build jdk21u HEAD weekly
    
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>
    
    * Currently only jdk-22 is build tag triggered
    
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>
    
    ---------
    
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/jobs/configurations/jdk21u.groovy
+++ pipelines/jobs/configurations/jdk21u.groovy
@@ -35,7 +35,7 @@ targetConfigurations = [
 // 23:30 Mon, Wed, Fri
 //Uses releaseTrigger_21ea: triggerSchedule_nightly = 'TZ=UTC\n30 23 * * 1,3,5'
 // 23:30 Sat
-//Replaced by releaseTrigger_21ea: triggerSchedule_weekly = 'TZ=UTC\n30 23 * * 6'
+triggerSchedule_weekly = 'TZ=UTC\n30 23 * * 6'
 
 // scmReferences to use for weekly release build
 weekly_release_scmReferences = [
