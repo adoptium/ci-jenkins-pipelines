b165a2d0c1b83682a810b308daebcb9a45007383
Andrew Leonard <anleonar@redhat.com>
2023-11-27 11:21:19 +0000

    Signing verifier job
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/build/common/verify_signing.groovy
+++ pipelines/build/common/verify_signing.groovy
@@ -38,7 +38,7 @@ String find_signtool() {
             exit 1
     }
 
-    def windowsKitPath = "/cygdrive/c/progra~2/wi3cf2~1"
+    def windowsKitPath = "/cygdrive/c/'Program Files (x86)'/'Windows Kits'"
 
     def files = sh(script:"find ${windowsKitPath} -type f -path */${arch}/signtool.exe", \
                    returnStdout:true).split("\\r?\\n|\\r")
@@ -49,6 +49,141 @@ String find_signtool() {
         exit 2
     } else {
         return files[0].trim()
+    }
+}
+
+// Unpack the archives so the signartures can be checked
+void unpackArchives(String unpack_dir, String[] archives) {
+    archives.each { archive ->
+        def dir = "${unpack_dir}/${archive}"
+        if (params.TARGET_OS == "mac") {
+            sh("mkdir -p ${dir} && tar -C ${dir} -xf *-${archive}*.tar.gz")
+        } else { // Windows
+            sh("mkdir -p ${dir} && unzip *-${archive}*.zip -d ${dir}")
+        }
+    }
+
+    // Copy JDK so it can be used for unpacking using jmod/jimage
+    sh("mkdir jdk_cp && cp -r ${unpack_dir}/jdk/*/* jdk_cp")
+
+    def jdk_bin = "jdk_cp/bin"
+    if (params.TARGET_OS == "mac") {
+        jdk_bin = "jdk_cp/Contents/Home/bin"
+    }
+
+    // Expand the JMODs and modules image to test binaries within
+    archives.each { archive ->
+        def dir = "${unpack_dir}/${archive}"
+        // Expand JMODs
+        println "Expanding JMODS under ${dir}"
+        def jmods = sh(script:"find ${dir} -type f -name '*.jmod'", \
+                       returnStdout:true).split("\\r?\\n|\\r")
+        jmods.each { jmod ->
+            if (jmod.trim() != "") {
+                def expand_dir = "expanded_" + sh(script:"basename ${jmod}", returnStdout:true)
+                expand_dir = "${dir}/${expand_dir}".trim()
+                sh("mkdir ${expand_dir}")
+                sh("${jdk_bin}/jmod extract --dir ${expand_dir} ${jmod}")
+            }
+        }
+
+        // Expand "modules" compress image containing jmods
+        println "Expanding 'modules' compressed image file under ${dir}"
+        def modules = sh(script:"find ${dir} -type f -name 'modules'", \
+                         returnStdout:true).split("\\r?\\n|\\r")
+        modules.each { module ->
+            if (module.trim() != "") {
+                def expand_dir = "expanded_" + sh(script:"basename ${module}", returnStdout:true)
+                expand_dir = "${dir}/${expand_dir}".trim()
+                sh("mkdir ${expand_dir}")
+                sh("${jdk_bin}/jimage extract --dir ${expand_dir} ${module}")
+            }
+        }
+    }
+}
+
+// Verify executables for Signatures
+void verifyExecutables(String unpack_dir) {
+    if (params.TARGET_OS == "mac") {
+        // On Mac find all dylib's and binaries marked as "executable",
+        // also add "jpackageapplauncher" specific case which is not marked as "executable"
+        // as it is within the jdk.jpackage resources used by jpackage util to generate user app launchers
+        def bins = sh(script:"find ${unpack_dir} -perm +111 -type f -not -name '.*' -o -name '*.dylib' -o -name 'jpackageapplauncher' || \
+                              find ${unpack_dir} -perm /111 -type f -not -name '.*' -o -name '*.dylib' -o -name 'jpackageapplauncher'",  \
+                      returnStdout:true).split("\\r?\\n|\\r")
+        bins.each { bin ->
+            if (bin.trim() != "") {
+                def rc = sh(script:"codesign --verify --verbose ${bin}", returnStatus:true)
+                if (rc != 0) {
+                    println "Error: executable not Signed: ${bin}"
+                    currentBuild.result = 'FAILURE'
+                } else {
+                    println "Signed correctly: ${bin}"
+                }
+            }
+        }
+    } else { // Windows
+        def signtool = find_signtool()
+
+        // Find all exe/dll's that must be Signed
+        def bins = sh(script:"find ${unpack_dir} -type f -name '*.exe' -o -name '*.dll'", \
+                      returnStdout:true).split("\\r?\\n|\\r")
+        bins.each { bin ->
+            if (bin.trim() != "") {
+                def rc = sh(script:"${signtool} verify /pa /v ${bin}", returnStatus:true)
+                if (rc != 0) {
+                    println "Error: executable not Signed: ${bin}"
+                    currentBuild.result = 'FAILURE'
+                } else {
+                    println "Signed correctly: ${bin}"
+                }
+            }
+        }
+    }
+}
+
+// Verify installers for Signatures and Notarization(mac only)
+void verifyInstallers() {
+    if (params.TARGET_OS == "mac") {
+        // Find all pkg's that need to be Signed and Notarized
+        def pkgs = sh(script:"find . -type f -name '*.pkg'", \
+                      returnStdout:true).split("\\r?\\n|\\r")
+        pkgs.each { pkg ->
+            if (pkg.trim() != "") {
+                def rc = sh(script:"pkgutil --check-signature ${pkg}", returnStatus:true)
+                if (rc != 0) {
+                    println "Error: pkg not Signed: ${pkg}"
+                    currentBuild.result = 'FAILURE'
+                } else {
+                    println "Signed correctly: ${pkg}"
+                }
+
+                rc = sh(script:"spctl -a -vvv -t install ${pkg}", returnStatus:true)
+                if (rc != 0) {
+                    println "Error: pkg not Notarized: ${pkg}"
+                    currentBuild.result = 'FAILURE'
+                } else {
+                    println "Notarized correctly: ${pkg}"
+                }
+            }
+        }
+    } else { // Windows
+        // Find all msi's that need to be Signed
+        def signtool = find_signtool()
+
+        def msis = sh(script:"find . -type f -name '*.msi'", \
+                      returnStdout:true).split("\\r?\\n|\\r")
+        msis.each { msi ->
+            if (msi.trim() != "") {
+                def rc = sh(script:"${signtool} verify /pa /v ${msi}", returnStatus:true)
+                if (rc != 0) {
+                    println "Error: installer not Signed: ${msi}"
+                    currentBuild.result = 'FAILURE'
+                } else {
+                    println "Signed correctly: ${msi}"
+                }
+            }
+        }
     }
 }
 
@@ -111,109 +246,13 @@ if (params.TARGET_OS != "mac" && params.TARGET_OS != "windows") {
                 // Unpack archives
                 def unpack_dir = "unpacked"
                 def archives = ["jdk", "jre"]
+                unpackArchives(unpack_dir, archives)
 
-                archives.each { archive ->
-                    def dir = "${unpack_dir}/${archive}"
-                    if (params.TARGET_OS == "mac") {
-                        sh("mkdir -p ${dir} && tar -C ${dir} -xf *-${archive}*.tar.gz")
-                    } else { // Windows
-                        sh("mkdir -p ${dir} && unzip *-${archive}*.zip -d ${dir}")
-                    }
-                }
+                // Verify all executables for Signatures
+                verifyExecutables(unpack_dir)
 
-                // Copy JDK so it can be used for unpacking using jmod/jimage
-                sh("mkdir jdk_cp && cp -r ${unpack_dir}/jdk/*/* jdk_cp")
-
-                def jdk_bin = "jdk_cp/bin"
-                if (params.TARGET_OS == "mac") {
-                    jdk_bin = "jdk_cp/Contents/Home/bin"
-                }
-
-                // Expand the JMODs and modules image to test binaries within
-                archives.each { archive ->
-                    def dir = "${unpack_dir}/${archive}"
-                    // Expand JMODs
-                    println "Expanding JMODS under ${dir}"
-                    def jmods = sh(script:"find ${dir} -type f -name '*.jmod'", \
-                                   returnStdout:true).split("\\r?\\n|\\r")
-                    jmods.each { jmod ->
-                        if (jmod.trim() != "") {
-                            def expand_dir = "expanded_" + sh(script:"basename ${jmod}", returnStdout:true)
-                            expand_dir = "${dir}/${expand_dir}".trim()
-                            sh("mkdir ${expand_dir}")
-                            sh("${jdk_bin}/jmod extract --dir ${expand_dir} ${jmod}")
-                        }
-                    }
-
-                    // Expand "modules" compress image containing jmods
-                    println "Expanding 'modules' compressed image file under ${dir}"
-                    def modules = sh(script:"find ${dir} -type f -name 'modules'", \
-                                     returnStdout:true).split("\\r?\\n|\\r")
-                    modules.each { module ->
-                        if (module.trim() != "") {
-                            def expand_dir = "expanded_" + sh(script:"basename ${module}", returnStdout:true)
-                            expand_dir = "${dir}/${expand_dir}".trim()
-                            sh("mkdir ${expand_dir}")
-                            sh("${jdk_bin}/jimage extract --dir ${expand_dir} ${module}")
-                        }
-                    }
-                }
-
-                // Verify all executables and installers for Signatures
-                if (params.TARGET_OS == "mac") {
-                    // On Mac find all dylib's and binaries marked as "executable" and .pkg's,
-                    // also add "jpackageapplauncher" specific case which is not marked as "executable"
-                    // as it is within the jdk.jpackage resources used by jpackage util to generate user app launchers
-                    def bins = sh(script:"find . -perm +111 -type f -not -name '.*' -o -name '*.dylib' -o -name 'jpackageapplauncher' -o -name '*.pkg' || \
-                                          find . -perm /111 -type f -not -name '.*' -o -name '*.dylib' -o -name 'jpackageapplauncher' -o -name '*.pkg'",  \
-                                  returnStdout:true).split("\\r?\\n|\\r")
-                    bins.each { bin ->
-                        if (bin.trim() != "") {
-                            def rc = sh(script:"codesign --verify --verbose ${bin}", returnStatus:true)
-                            if (rc != 0) {
-                                println "Error: dylib not signed: ${bin}"
-                                currentBuild.result = 'FAILURE'
-                            } else {
-                                println "Signed correctly: ${bin}"
-                            }
-                        }
-                    }
-                } else { // Windows
-                    def signtool = find_signtool()
-
-                    // Find all exe/dll's and msi's that must be Signed
-                    def bins = sh(script:"find ${unpack_dir} -type f -name '*.exe' -o -name '*.dll' -o -name '*.msi'", \
-                                  returnStdout:true).split("\\r?\\n|\\r")
-                    bins.each { bin ->
-                        if (bin.trim() != "") {
-                            def rc = sh(script:"${signtool} verify /pa /v ${bin}", returnStatus:true)
-                            if (rc != 0) {
-                                println "Error: binary not signed: ${bin}"
-                                currentBuild.result = 'FAILURE'
-                            } else { 
-                                println "Signed correctly: ${bin}"
-                            }
-                        }
-                    }
-                }
-
-                // For Mac also verify installer (if built) is Notarized
-                if (params.TARGET_OS == "mac") {
-                    // Find all pkg's that need to be Notarized
-                    def pkgs = sh(script:"find . -type f -name '*.pkg'", \
-                                  returnStdout:true).split("\\r?\\n|\\r") 
-                    pkgs.each { pkg ->
-                        if (pkg.trim() != "") {
-                            def rc = sh(script:"spctl -a -vvv -t install ${pkg}", returnStatus:true)
-                            if (rc != 0) {
-                                println "Error: pkg not Notarized: ${pkg}"
-                                currentBuild.result = 'FAILURE'
-                            } else {
-                                println "Notarized correctly: ${pkg}"
-                            }
-                        }
-                    }
-                }
+                // Verify installers (if built) are Signed and Notarized(mac only)
+                verifyInstallers()
             } finally {
                 // Clean workspace afterwards
                 cleanWs notFailBuild: true, disableDeferredWipeout: true, deleteDirs: true
