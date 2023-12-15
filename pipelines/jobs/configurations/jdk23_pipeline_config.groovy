8d53341710838f15d05637684bec462ed201f2fc
Stewart X Addison <sxa@redhat.com>
2023-12-15 12:39:37 +0000

    Add JDK23 pipeline configuration
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

---
+++ pipelines/jobs/configurations/jdk23_pipeline_config.groovy
@@ -0,0 +1,170 @@
+class Config23 {
+
+    final Map<String, Map<String, ?>> buildConfigurations = [
+        x64Mac    : [
+                os                  : 'mac',
+                arch                : 'x64',
+                additionalNodeLabels: 'xcode15.0.1',
+                additionalTestLabels: [
+                        openj9      : '!sw.os.osx.10_11'
+                ],
+                test                : 'default',
+                configureArgs       : '--enable-dtrace',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        x64Linux  : [
+                os                  : 'linux',
+                arch                : 'x64',
+                dockerImage         : 'adoptopenjdk/centos7_build_image',
+                dockerFile: [
+                        openj9      : 'pipelines/build/dockerFiles/cuda.dockerfile'
+                ],
+                test                : 'default',
+                additionalTestLabels: [
+                        openj9      : '!(centos6||rhel6)'
+                ],
+                configureArgs       : [
+                        'openj9'    : '--enable-dtrace',
+                        'temurin'   : '--enable-dtrace'
+                ],
+                buildArgs           : [
+                        'temurin'   : '--create-source-archive --create-jre-image --create-sbom'
+                ]
+        ],
+
+        x64AlpineLinux  : [
+                os                  : 'alpine-linux',
+                arch                : 'x64',
+                dockerImage         : 'adoptopenjdk/alpine3_build_image',
+                test                : 'default',
+                configureArgs       : '--enable-headless-only=yes',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        aarch64AlpineLinux  : [
+                os                  : 'alpine-linux',
+                arch                : 'aarch64',
+                dockerImage         : 'adoptopenjdk/alpine3_build_image',
+                test                : 'default',
+                configureArgs       : '--enable-headless-only=yes',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        x64Windows: [
+                os                  : 'windows',
+                arch                : 'x64',
+                additionalNodeLabels: 'win2022&&vs2022',
+                test                : 'default',
+                configureArgs       : "--with-ucrt-dll-dir='C:/progra~2/wi3cf2~1/10/Redist/10.0.22621.0/ucrt/DLLs/x64'",
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        ppc64Aix    : [
+                os                  : 'aix',
+                arch                : 'ppc64',
+                additionalNodeLabels: [
+                        temurin: 'xlc16&&aix720',
+                        openj9:  'xlc16&&aix715'
+                ],
+                test                : 'default',
+                additionalTestLabels: [
+                        temurin      : 'sw.os.aix.7_2'
+                ],
+                cleanWorkspaceAfterBuild: true,
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        s390xLinux    : [
+                os                  : 'linux',
+                arch                : 's390x',
+                dockerImage         : 'rhel7_build_image',
+                test                : 'default',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        ppc64leLinux    : [
+                os                  : 'linux',
+                arch                : 'ppc64le',
+                dockerImage         : 'adoptopenjdk/centos7_build_image',
+                test                : 'default',
+                configureArgs       : [
+                        'openj9'      : '--enable-dtrace'
+                ],
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        aarch64Linux    : [
+                os                  : 'linux',
+                arch                : 'aarch64',
+                dockerImage         : 'adoptopenjdk/centos7_build_image',
+                test                : 'default',
+                configureArgs : '--enable-dtrace',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        aarch64Mac: [
+                os                  : 'mac',
+                arch                : 'aarch64',
+                additionalNodeLabels: 'xcode15.0.1',
+                test                : 'default',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        arm32Linux    : [
+                os                  : 'linux',
+                arch                : 'arm',
+                crossCompile        : 'aarch64',
+                dockerImage         : 'adoptopenjdk/ubuntu1604_build_image',
+                dockerArgs          : '--platform linux/arm/v7',
+                test                : 'default',
+                configureArgs       : '--enable-dtrace',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        riscv64Linux      :  [
+                os                  : 'linux',
+                arch                : 'riscv64',
+                test                : 'default',
+                configureArgs       : '--enable-dtrace',
+                buildArgs           : [
+                        'temurin'   : '--create-jre-image --create-sbom'
+                ]
+        ],
+
+        aarch64Windows: [
+                os                  : 'windows',
+                arch                : 'aarch64',
+                crossCompile        : 'x64',
+                additionalNodeLabels: 'win2022&&vs2022',
+                test                : 'default',
+                buildArgs       : [
+                        'temurin'   : '--create-jre-image --create-sbom --cross-compile'
+                ]
+        ]
+  ]
+
+}
+
+Config23 config = new Config23()
+return config.buildConfigurations
