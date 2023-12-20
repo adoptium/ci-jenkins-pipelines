1a7c54ca60d0030bf34fd0e227e3a68bae708a07
Ludovic Henry <git@ludovic.dev>
2023-12-20 11:04:58 +0000

    Use dockerhost-rise-ubuntu2204-aarch64-1 host to run in QEMU for riscv64

--- pipelines/jobs/configurations/jdk20u_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk20u_pipeline_config.groovy
@@ -146,7 +146,7 @@ class Config20 {
         riscv64Linux      :  [
                 os                  : 'linux',
                 arch                : 'riscv64',
-                crossCompile        : 'x64',
+                crossCompile        : 'dockerhost-rise-ubuntu2204-aarch64-1'
                 dockerImage         : 'adoptopenjdk/ubuntu2004_build_image:linux-riscv64',
                 dockerArgs          : '--platform linux/riscv64',
                 test                : 'default',
--- pipelines/jobs/configurations/jdk21u_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk21u_pipeline_config.groovy
@@ -166,6 +166,9 @@ class Config21 {
         riscv64Linux      :  [
                 os                  : 'linux',
                 arch                : 'riscv64',
+                // crossCompile        : 'dockerhost-rise-ubuntu2204-aarch64-1',
+                // dockerImage         : 'adoptopenjdk/ubuntu2004_build_image:linux-riscv64',
+                // dockerArgs          : '--platform linux/riscv64',
                 test                : 'default',
                 configureArgs       : '--enable-dtrace',
                 buildArgs           : [
