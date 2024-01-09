f5a1dc34093212bfe53f20939bb137c9b7dafdd5
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2024-01-09 15:56:33 +0000

    Allow : in build docker image name (#881)
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -1890,9 +1890,9 @@ class Build {
 
 
                         context.println "[NODE SHIFT] MOVING INTO DOCKER NODE MATCHING LABELNAME ${label}..."
-                        if ( ! ( "${buildConfig.DOCKER_IMAGE}" ==~ /^[A-Za-z0-9\/\.-_]*$/ ) ||
-                             ! ( "${buildConfig.DOCKER_ARGS}"  ==~ /^[A-Za-z0-9\/\.-_]*$/ ) ) {
-                             throw new Exception("[ERROR] Dubious characters in DOCKER* parameters ${buildConfig.DOCKER_IMAGE}/${buildConfig.DOCKER_ARGS} - aborting");
+                        if ( ! ( "${buildConfig.DOCKER_IMAGE}" ==~ /^[A-Za-z0-9\/\.\-_:]*$/ ) ||
+                             ! ( "${buildConfig.DOCKER_ARGS}"  ==~ /^[A-Za-z0-9\/\.\-_\ ]*$/ ) ) {
+                             throw new Exception("[ERROR] Dubious characters in DOCKER* image or parameters: ${buildConfig.DOCKER_IMAGE} ${buildConfig.DOCKER_ARGS} - aborting");
                         }
                         context.node(label) {
                             addNodeToBuildDescription()
