191a02644f0f56464e552ea7fa0340bbce51ce4f
Andrew Leonard <anleonar@redhat.com>
2024-02-01 16:26:48 +0000

    Add trigger_beta_build.groovy jenkins file
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/build/common/trigger_beta_build.groovy
+++ pipelines/build/common/trigger_beta_build.groovy
@@ -40,10 +40,10 @@ node('worker') {
     if (!params.FORCE) {
         // Check binaries repo for existance of the given release?
         def desiredRepoTagURL="${binariesRepo}/releases/tag/${buildTag}"
-        def httpCode=sh(script:'curl -s -o /dev/null -w "%{http_code}" "'+desiredRepoTagURL+'"', returnStdout:true)
-        if (httpCode == 200) {
+        def httpCode=sh(script:'curl -s -o /dev/null -w \"%{http_code}\" \"'+desiredRepoTagURL+'\"', returnStdout:true)
+        if (httpCode == "200") {
             echo "Release $buildTag already published - nothing to do"
-        } else if (httpCode == 404) {
+        } else if (httpCode == "404") {
             echo "New unpublished build tag ${buildTag} - triggering build"
             triggerBuild = true
         } else {
