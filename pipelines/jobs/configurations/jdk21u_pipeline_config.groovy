b270ee063ef91ae7b7301b1eac4aeb08a97b26e6
Shelley Lambert <slambert@gmail.com>
2024-02-14 10:58:17 -0500

    Add dev.system for JDK21+
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- pipelines/jobs/configurations/jdk21u_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk21u_pipeline_config.groovy
@@ -26,7 +26,7 @@ class Config21 {
                         openj9      : 'pipelines/build/dockerFiles/cuda.dockerfile'
                 ],
                 test: [
-                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.functional', 'extended.functional’, ’extended.openjdk', 'extended.perf', 'special.functional', 'sanity.external', 'dev.openjdk', 'dev.functional']
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.functional', 'extended.functional’, ’extended.openjdk', 'extended.perf', 'special.functional', 'sanity.external', 'dev.openjdk', 'dev.functional', 'dev.system']
                 ],
                 reproducibleCompare : [
                         'temurin'   : true
--- pipelines/jobs/configurations/jdk22_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk22_pipeline_config.groovy
@@ -23,7 +23,7 @@ class Config22 {
                         openj9      : 'pipelines/build/dockerFiles/cuda.dockerfile'
                 ],
                 test: [
-                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.functional', 'extended.functional’, ’extended.openjdk', 'extended.perf', 'special.functional', 'sanity.external', 'dev.openjdk', 'dev.functional']
+                        weekly : ['sanity.openjdk', 'sanity.system', 'extended.system', 'sanity.perf', 'sanity.functional', 'extended.functional’, ’extended.openjdk', 'extended.perf', 'special.functional', 'sanity.external', 'dev.openjdk', 'dev.functional', 'dev.system']
                 ],
                 additionalTestLabels: [
                         openj9      : '!(centos6||rhel6)'
