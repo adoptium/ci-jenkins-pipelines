ffb010518b2fbaa2ad5b8ceb09437fa352fa70f0
Andrew Leonard <anleonar@redhat.com>
2024-02-19 13:58:17 +0000

    Don't auto trigger beta EA builds with testing during a release
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/build/common/trigger_beta_build.groovy
+++ pipelines/build/common/trigger_beta_build.groovy
@@ -35,7 +35,7 @@ def binariesRepo="https://github.com/${params.BINARIES_REPO}".replaceAll("_NN_",
 
 def triggerMainBuild = false
 def triggerEvaluationBuild = false
-def enableTesting = true
+def enableTesting = false
 def overrideMainTargetConfigurations = ""
 def overrideEvaluationTargetConfigurations = ""
 
