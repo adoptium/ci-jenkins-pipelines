6f67bec444149fea83ab8cb826d316346048ee7e
Andrew Leonard <anleonar@redhat.com>
2024-02-23 18:24:59 +0000

    Add variant and ability to ignore a target platform
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/build/common/trigger_beta_build.groovy
+++ pipelines/build/common/trigger_beta_build.groovy
@@ -112,7 +112,7 @@ def loadTargetConfigurations(String javaVersion, String variant, String configSe
 
     println "targetConfigurations for variant ${variant} = " + JsonOutput.prettyPrint(JsonOutput.toJson(targetConfigurationsForVariant))
 
-    return targetConfigurationsForVariant
+    return JsonOutput.toJson(targetConfigurationsForVariant)
 }
 
 node('worker') {
@@ -247,10 +247,10 @@ if (triggerMainBuild || triggerEvaluationBuild) {
 
                     // Specify the required targetConfigurations
                     if (pipeline_type == "main") {
-                        jobParams.add(text(name: 'targetConfigurations',     JsonOutput.prettyPrint(JsonOutput.toJson(mainTargetConfigurations))))
+                        jobParams.add(text(name: 'targetConfigurations',     JsonOutput.prettyPrint(mainTargetConfigurations)))
                     }
                     if (pipeline_type == "evaluation") {
-                        jobParams.add(text(name: 'targetConfigurations',     JsonOutput.prettyPrint(JsonOutput.toJson(evaluationTargetConfigurations))))
+                        jobParams.add(text(name: 'targetConfigurations',     JsonOutput.prettyPrint(evaluationTargetConfigurations)))
                     }
 
                     def job = build job: "${pipeline}", propagate: true, parameters: jobParams
