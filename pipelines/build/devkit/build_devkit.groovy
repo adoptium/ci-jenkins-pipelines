95365ba6d460e3041ccd6c1cf189209cd2ec8091
Andrew Leonard <anleonar@redhat.com>
2024-02-29 13:01:29 +0000

    Build a linux devkit
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

---
+++ pipelines/build/devkit/build_devkit.groovy
@@ -0,0 +1,41 @@
+package common
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+def build_devkit() {
+    def openjdkRepo = "https://github.com/openjdk/${params.VERSION}.git"
+ 
+    checkout scm
+ 
+    sh(script:"git clone ${openjdkRepo} ${params.VERSION}")
+
+    sh(script:"cp pipelines/build/devkit/binutils-2.39.patch ${params.VERSION}/make/devkit/patches/${params.ARCH}-binutils-2.39.patch")
+
+    sh(script:"cd ${params.VERSION} && patch -p1<../pipelines/build/devkit/Tools.gmk.patch")
+}
+
+node(params.DEVKIT_BUILD_NODE) {
+  try {
+    context.cleanWs notFailBuild: true, disableDeferredWipeout: true, deleteDirs: true
+
+    docker.image(params.DOCKER_IMAGE).pull()
+
+    docker.image(params.DOCKER_IMAGE).inside() {
+        build_devkit()
+    }
+  } finally { 
+    cleanWs notFailBuild: true
+  } 
+}
+
