7176a3da464823d79b06f912da11a6643e051d04
Andrew Leonard <anleonar@redhat.com>
2024-03-28 10:08:05 +0000

    Enable build x64/ppc64le/aarch64 Linux for jdk-21+ using Adoptium DevKit
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/jobs/configurations/jdk22_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk22_pipeline_config.groovy
@@ -34,7 +34,7 @@ class Config22 {
                         'temurin'   : '--enable-dtrace'
                 ],
                 buildArgs           : [
-                        'temurin'   : '--create-source-archive --create-jre-image --create-sbom'
+                        'temurin'   : '--create-source-archive --create-jre-image --create-sbom --use-adoptium-devkit gcc-11.3.0-Centos7.9.2009-b02'
                 ]
         ],
 
@@ -107,7 +107,7 @@ class Config22 {
                         'openj9'      : '--enable-dtrace'
                 ],
                 buildArgs           : [
-                        'temurin'   : '--create-jre-image --create-sbom'
+                        'temurin'   : '--create-jre-image --create-sbom --use-adoptium-devkit gcc-11.3.0-Centos7.9.2009-b02'
                 ]
         ],
 
@@ -118,7 +118,7 @@ class Config22 {
                 test                : 'default',
                 configureArgs : '--enable-dtrace',
                 buildArgs           : [
-                        'temurin'   : '--create-jre-image --create-sbom'
+                        'temurin'   : '--create-jre-image --create-sbom --use-adoptium-devkit gcc-11.3.0-Centos7.6.1810-b02'
                 ]
         ],
 
