e97301926a1f087d2e881564af435476658f872c
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2024-04-06 00:47:27 +0100

    add jdk22u build pipelines (#993)
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

---
+++ pipelines/jobs/configurations/jdk22u_evaluation.groovy
@@ -0,0 +1,13 @@
+targetConfigurations = [
+        'aarch64Windows' : [
+                'temurin'
+        ]
+]
+
+// scmReferences to use for weekly evaluation release build
+weekly_evaluation_scmReferences = [
+        'hotspot'        : '',
+        'temurin'        : ''
+]
+
+return this
