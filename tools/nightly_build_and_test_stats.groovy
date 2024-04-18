98de643192d284a4ae98d64652b92a6d3d34f0dd
Andrew Leonard <anleonar@redhat.com>
2024-04-18 16:02:04 +0100

    Update daily EA build Slack status to filter -ga build tags
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- tools/nightly_build_and_test_stats.groovy
+++ tools/nightly_build_and_test_stats.groovy
@@ -62,7 +62,7 @@ def getLatestOpenjdkBuildTag(String version, Integer headMinus) {
         jdk8Filter += " | grep '\\-aarch32\\-'"
     }
 
-    def latestTag = sh(returnStdout: true, script:"git ls-remote --sort=-v:refname --tags ${openjdkRepo} | grep -v '\\^{}' | tr -s '\\t ' ' ' | cut -d' ' -f2 | sed \"s,refs/tags/,,\" | grep -v '\\-ga' ${jdk8Filter} | sort -V -r | head ${headMinus} | tr -d '\\n'")
+    def latestTag = sh(returnStdout: true, script:"git ls-remote --sort=-v:refname --tags ${openjdkRepo} | grep -v '\\^{}' | tr -s '\\t ' ' ' | cut -d' ' -f2 | sed \"s,refs/tags/,,\" | grep -v '\\-ga' ${jdk8Filter} | sort -V -r | head ${headMinus} | tail -1 | tr -d '\\n'")
 
     echo "latest(head ${headMinus}) upstream openjdk/${version} tag = ${latestTag}"
 
