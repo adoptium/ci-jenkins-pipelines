1afcd40fcd18fd24a436ae8db377e0041f0ae814
Adam Farley <adfarley@redhat.com>
2024-10-09 16:45:38 +0100

    A better test to see if testing was enabled in a given pipeline
    Signed-off-by: Adam Farley <adfarley@redhat.com>

--- tools/nightly_build_and_test_stats.groovy
+++ tools/nightly_build_and_test_stats.groovy
@@ -805,7 +805,7 @@ node('worker') {
                     }
 
                     def testsShouldHaveRun = false
-                    if ( probableBuildUrl != "" && sh(returnStdout: true, script: "wget -q -O - ${trssUrl}/api/getBuildHistory?buildUrl=${probableBuildUrl}").count("\\\"enableTests\\\",\\\"value\\\": true") == 1 ) {
+                    if ( probableBuildUrl != "" && sh(returnStdout: true, script: "wget -q -O - ${trssUrl}/api/getBuildHistory?buildUrl=${probableBuildUrl}") ==~ /.name.:.enableTests.,.value.:true/ ) {
                         testsShouldHaveRun = true
                     }
                     if (reproducibleBuilds.containsKey(featureRelease)) {
