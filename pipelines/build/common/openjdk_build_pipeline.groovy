ccc2ce7deb8b665436f8bd655d93d1ae2f5c7227
Andrew Leonard <anleonar@redhat.com>
2024-12-05 15:56:01 +0000

    Prevent jdk11+ from running external sign.sh as already signed during build
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -748,7 +748,7 @@ class Build {
     /*
     Run the Sign downstream job. We run this job on windows and jdk8 hotspot & jdk13 mac builds.
     The job code signs and notarizes the binaries so they can run on these operating systems without encountering issues.
-    tarball signing only required for jdk8u, as jdk11+ is signed dynamically during the build.
+    Tarball signing only required for jdk8u, as jdk11+ is signed dynamically during the build.
     */
     def sign(VersionInfo versionInfo) {
         // Sign and archive jobs if needed
