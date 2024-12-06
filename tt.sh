79eb4189c7ef717626e40dc1ab3992fadda39ddf
Andrew Leonard <anleonar@redhat.com>
2024-12-06 16:42:07 +0000

    Prevent jdk11+ from running external sign.sh as already signed during build
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- tt.sh
+++
@@ -1,6 +0,0 @@
-
-file="api-ms-winsdfsdfsf.dll"
-if [[ "$file" =~ api-ms-win.* ]] || [[ "$file" =~ msvcp.* ]] || [[ "$file" =~ ucrtbase.* ]] || [[ "$file" =~ vcruntime.* ]]; then
-                                                            echo "Skipping Microsoft file $file"
-fi
-
