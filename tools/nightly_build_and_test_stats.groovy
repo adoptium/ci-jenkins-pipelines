a3fe2bc1a80c5b1fa76e274facf00f5937f98fde
Andrew Leonard <anleonar@redhat.com>
2025-02-07 10:54:38 +0000

    Update EA build status to not find GA builds
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- tools/nightly_build_and_test_stats.groovy
+++ tools/nightly_build_and_test_stats.groovy
@@ -314,7 +314,6 @@ def getBuildUrls(String trssUrl, String variant, String featureRelease, String p
 
     if (pipelineJson.size() > 0) {
         def foundBuildTimestamp = 0
-echo "BUILDURLS: "+pipelineJson
         pipelineJson.each { job ->
             def overridePublishName = ""
             def buildScmRef = ""
@@ -334,22 +333,12 @@ echo "BUILDURLS: "+pipelineJson
                     releaseType = buildParam.value
                 }
             }
-echo "B: "+job
-echo "BB:"+releaseType
-echo "BB:"+containsVariant
-echo "BB:"+overridePublishName
-echo "BB:"+buildScmRef
-echo "BB:"+job.status
-echo "BB RS:"+requiredStatus+"BB"
-echo "BB:"+featureReleaseInt
-echo "BB:"+containsX64AlpineLinux
-echo "BB:"+featureRelease
+
             // Is there a job for the required tag?
             if (releaseType == "Weekly" && containsVariant && overridePublishName == publishName && buildScmRef == scmRef && job.status != null && (requiredStatus == "" || job.status == requiredStatus)) {
                 if (featureReleaseInt == 8) {
                     // alpine-jdk8u cannot be distinguished from jdk8u by the scmRef alone, so check for "x64AlpineLinux" in the targetConfiguration
                     if ((featureRelease == "alpine-jdk8u" && containsX64AlpineLinux) || (featureRelease != "alpine-jdk8u" && !containsX64AlpineLinux)) {
-echo "AAA"
                         if (job.timestamp > foundBuildTimestamp || !latestOnly) {
                             if (latestOnly) {
                                 functionBuildUrls = [[job.buildUrl, job._id, job.status]]
