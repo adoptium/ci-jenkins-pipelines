e5d105838eceb67cec31c23bfaea3ea57c5c50dd
Stewart X Addison <sxa@redhat.com>
2025-03-03 17:49:43 +0000

    Add fedora versions link
    Signed-off-by: Stewart X Addison <sxa@redhat.com>

--- pipelines/build/devkit/make_devkit.sh
+++ pipelines/build/devkit/make_devkit.sh
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
+# For Fedora see https://mirrormanager.fedoraproject.org/ for a lsit of versions/architectures
+
 
 set -e
 
