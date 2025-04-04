a489141fd43d32e18592cc8bd451a84237f5f41e
Andrew Leonard <anleonar@redhat.com>
2025-04-04 11:22:42 +0100

    Fix Windows sha256 retrieval
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -1436,11 +1436,12 @@ class Build {
                                             """.stripIndent(), returnStdout: true, returnStatus: false).replaceAll('\n', '')
             } else {
                 context.println "Windows detected - running bat to generate SHA256 sums in writeMetadata"
-                String hash_stdout = context.bat(script: "sha256sum ${file}", returnStdout: true, returnStatus: false)
-                // Windows batch returns stdout of <command invoked>\r\n<sha256sum output>\r\n
-                hash = hash_stdout.split('\r').last().replaceAll('\n', '').split(' ').first()
+                //String hash_stdout = context.bat(script: "sha256sum ${file}", returnStdout: true, returnStatus: false)
+                //// Windows batch returns stdout of <command invoked>\r\n<sha256sum output>\r\n
+                //hash = hash_stdout.split('\r').last().replaceAll('\n', '').split(' ').first()
+                hash = context.bat(script: "@sha256sum ${file}", returnStdout: true, returnStatus: false)
             }
-            context.println "archive sha256 = ${hash}"
+            context.println "archive sha256 = /${hash}/"
 
             data.binary_type = type
             data.sha256 = hash
