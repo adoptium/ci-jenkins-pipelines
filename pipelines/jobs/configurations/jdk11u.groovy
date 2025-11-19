98c06f2c13476682cbfc87978f438b3c35ca8e04
Stewart X Addison <sxa@ibm.com>
2025-11-19 10:19:44 +0000

    Add hotspot variants to AIX/xlinux/zlinux for regular job generation
    Signed-off-by: Stewart X Addison <sxa@ibm.com>

--- pipelines/jobs/configurations/jdk11u.groovy
+++ pipelines/jobs/configurations/jdk11u.groovy
@@ -1,12 +1,12 @@
 targetConfigurations = [
         'x64Mac'        : [    'temurin',    'openj9'                    ],
-        'x64Linux'      : [    'temurin',    'openj9',    'dragonwell',    'corretto',    'bisheng',    'fast_startup'],
+        'x64Linux'      : [    'temurin',    'hotspot',    'dragonwell',    'corretto',    'bisheng',    'fast_startup'],
         'x64AlpineLinux': [    'temurin'                            ],
         'x64Windows'    : [    'temurin',    'openj9',    'dragonwell'            ],
         'x32Windows'    : [    'temurin'                            ],
-        'ppc64Aix'      : [    'temurin',    'openj9'                    ],
+        'ppc64Aix'      : [    'temurin',    'hotspot'                   ],
         'ppc64leLinux'  : [    'temurin',    'openj9'                    ],
-        's390xLinux'    : [    'temurin',    'openj9'                    ],
+        's390xLinux'    : [    'temurin',    'hotspot'                   ],
         'aarch64Linux'  : [    'temurin',    'openj9',    'dragonwell',                   'bisheng'    ],
         'aarch64Mac'    : [    'temurin',                           ],
         'arm32Linux'    : [    'temurin'                            ]
