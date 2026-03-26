3136c1bcf171d83556e39431e07b75e6e02c1d94
Adam Farley <adfarley@ibm.com>
2026-03-26 13:08:54 +0000

    Removing another temporary change
    Signed-off-by: Adam Farley <adfarley@ibm.com>

--- pipelines/jobs/configurations/jdk21u_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk21u_pipeline_config.groovy
@@ -14,9 +14,6 @@ class Config21 {
                 configureArgs       : '--enable-dtrace',
                 buildArgs           : [
                         'temurin'   : '--create-jre-image --create-sbom'
-                ],
-                additionalTestParams: [
-                        temurin     : [TIME_LIMIT: '30']
                 ]
         ],
 
